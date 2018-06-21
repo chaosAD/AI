761a22410fa5bd3fcf199b913966975b69981f19
kaosad <frasker@hotmail.com>
2018-06-21 10:16:53 +0800

    Created AI project

---
+++ src/AI.h
@@ -0,0 +1,5 @@
+#ifndef _AI_H
+#define _AI_H
+
+
+#endif // _AI_H
---
+++ test/test_AI.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "AI.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_AI_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement AI");
+}
